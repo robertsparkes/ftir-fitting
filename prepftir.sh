ac3934061956e5571aba9dcbc87829f3e79b6b36
Robert Sparkes <r.sparkes@mmu.ac.uk>
2023-06-30 12:39:11 +0100

    Add files via upload

---
+++ prepftir.sh
@@ -0,0 +1,8 @@
+mkdir combinedfigure
+mkdir pdf
+mkdir jpg
+mkdir param
+mkdir xy_chart_files
+
+echo "name pvc_height carbonate_height dotp_height p4008_height pvc_perc carbonate_perc dotp_perc p4008_perc" > acombinedresults.txt
+
